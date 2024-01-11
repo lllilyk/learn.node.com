0de8e304d7df3c4575eaa2f181791a8bfce44e8f
lllilyk <ym0402kkk@naver.com>
2024-01-11 23:25:25 +0900

    next js(13 app dir)와 prisma 연동

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
 
         console.log("email:", formData.email, "/ password:", formData.password);
 
-        const response = await fetch('../api/auth/login', { 
+        const response = await fetch('/api/auth/login', { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
