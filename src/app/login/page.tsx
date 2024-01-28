ac33a1f9fe68d590642d29faffc35155249ca3c7
lllilyk <ym0402kkk@naver.com>
2024-01-28 22:44:01 +0900

    CORS 정책 설정을 위한 엔드포인트 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
 
         console.log("email:", formData.email, "/ password:", formData.password);
 
-        const response = await fetch('/api/auth/login', { 
+        const response = await fetch('http://localhost:3001/api/v1/auth/login', { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -32,6 +32,8 @@ export default function Login() {
         });
     
         const data = await response.json();
+
+        localStorage.setItem("accessToken", data.accessToken);
     };
     
     return (
